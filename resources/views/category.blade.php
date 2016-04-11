a4108d8658c05adcfab79047f850bec87f3efe0a
Zlywilk <mylfu1@gmail.com>
2016-04-11 21:53:06 +0200

    fix path to infomation about markers

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -8,7 +8,7 @@
     @if($item->opis == '')
     <div class="col-xs-8 col-md-4"> <p>{{$item->title}}</p></div>
     @else
-<div class="col-xs-8 col-md-4"> <a class="ajax" href="/markers/{{$item->id}}">{{$item->title}}</a><br></div>
+<div class="col-xs-8 col-md-4"> <a class="ajax" href="markers/{{$item->id}}">{{$item->title}}</a><br></div>
     @endif
     @endforeach
 </div>
